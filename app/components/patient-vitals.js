943a0ed88f246630bdbc51d738c8b5a0bf681636
Arewa Olakunle <danceasarxx@gmail.com>
2018-01-29 10:44:59 +0100

    Vitals can neither be edited nor deleted
    This is because the component didn't have access to the proper a
    ctions. So I propagated the edit and delete actions from the visit
    controller to the vitals component.

--- app/components/patient-vitals.js
+++ app/components/patient-vitals.js
@@ -1,12 +1,23 @@
 import Ember from 'ember';
+import ModalHelper from 'hospitalrun/mixins/modal-helper';
 import UserSession from 'hospitalrun/mixins/user-session';
 
-export default Ember.Component.extend(UserSession, {
+export default Ember.Component.extend(UserSession, ModalHelper, {
   canAddVitals: function() {
     return this.currentUserCan('add_vitals');
   }.property(),
 
   canDeleteVitals: function() {
     return this.currentUserCan('delete_vitals');
-  }.property()
+  }.property(),
+
+  actions: {
+    showEditVitals(vitals) {
+      this.editVitals(vitals);
+    },
+
+    showDeleteVitals(vitals) {
+      this.deleteVitals(vitals);
+    }
+  }
 });
