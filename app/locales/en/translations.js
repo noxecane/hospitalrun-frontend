f98270348552e8229b71ffee9cb764bf8f27c539
Arewa Olakunle <danceasarxx@gmail.com>
2018-02-01 01:04:45 +0100

    The software still references its open source root
    Changed the names to Lafiya and added tsaron logo. Also redesigned
    the front page to better align our logo.

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -423,8 +423,8 @@ export default {
   },
   dashboard: {
     needs_user_setup: 'We recommend that you setup a User account.',
-    standalone_welcome: '<h4>Thanks for downloading HospitalRun</h4><p>You are running HospitalRun in stand alone mode. This mode allows you to support multiple users on a single, desktop/laptop instance of HospitalRun. This is ideal for:</p><ul><li>Evaluating HospitalRun for an eventual server deployment.</li><li>Using the platform to support a clinic / facility where a single instance is sufficient.</li></ul><p>If you\'re considering a multi-device deployment of HospitalRun, we\'re <a href="https://github.com/HospitalRun/hospitalrun-frontend/issues/1048" target="_blank">working on features</a> that will allow you to "graduate" from this single instance into a traditional cloud / server-based deployment.</p>',
-    title: 'Welcome to HospitalRun!'
+    standalone_welcome: '<h4>Thanks for downloading Lafiya</h4><p>You are running Lafiya in stand alone mode. This mode allows you to support multiple users on a single, desktop/laptop instance of Lafiya. This is ideal for:</p><ul><li>Evaluating Lafiya for an eventual server deployment.</li><li>Using the platform to support a clinic / facility where a single instance is sufficient.</li></ul><p>If you\'re considering a multi-device deployment of Lafiya, we\'re <a href="https://github.com/tsaron/lafiya/" target="_blank">working on features</a> that will allow you to "graduate" from this single instance into a traditional cloud / server-based deployment.</p>',
+    title: 'Welcome to Lafiya!'
   },
   dates: {
     'long': '{{years}} year {{months}} months {{days}} days',
@@ -1080,7 +1080,7 @@ export default {
     } }
   },
   navigation: {
-    about: 'About HospitalRun',
+    about: 'About Lafiya',
     actions: {
       login: 'Login',
       logout: 'Logout',
