1b9dd9acdbf573837e9fd2516b7815bbcef615ea
Arewa Olakunle <danceasarxx@gmail.com>
2018-02-17 11:42:05 +0100

    Fixed a quick bug

--- app/patients/edit/controller.js
+++ app/patients/edit/controller.js
@@ -90,7 +90,6 @@ export default AbstractEditController.extend(AllergyActions, BloodTypes, Diagnos
       'Charity',
       'Private',
       'Managed'
-      ];
     ];
     return types.map((type) => {
       return i18n.t(`patients.labels.patientType${type}`);
